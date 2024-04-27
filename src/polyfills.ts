85cdc58d47eaf52167b7b5236134ad1b6792a56f
Kreash <anton.kreash@gmail.com>
2024-04-27 18:09:37 +0300

    update Angular to version 14

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -15,37 +15,17 @@
  */
 
 /***************************************************************************************************
-* BROWSER POLYFILLS
-*/
-
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/set';  // Run `npm install --save classlist.js`.  // Run `npm install --save web-animations-js`.
-
+ * BROWSER POLYFILLS
+ */
 
 /** Evergreen browsers require these. **/
 // import 'core-js/es/reflect';
-import "core-js/proposals/reflect-metadata"
-
-
+// import 'core-js/stable/structured-clone';
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js';  // Included with Angular CLI.
-
-
+import 'zone.js'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
@@ -56,4 +36,3 @@ import 'zone.js';  // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
-
